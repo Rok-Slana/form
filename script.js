74c8dc2f5372d25b4ec527974be7bb2679e90a5e
Rok Slana <rokslana@gmail.com>
2022-10-11 20:13:00 +0200

    Final form

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-console.log('test');
+const pwdRegex = document.getElementById('pwd');
 
-function passwordValidation(){
-    const password1 = document.getElementById('pwd').innerText;
-    const password2 = document.getElementById('pwdVal').innerText;
+pwdRegex.addEventListener('input', (e) => {
+    let pwdRegexString = pwdRegex.value;
+    if(!/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/.test(pwdRegexString)){
+        console.log('Hi!')
+        pwdRegex.setCustomValidity('Password must be at least 8 characters long and contain one upper case letter, one lower case letter and one number');
+        pwdRegex.reportValidity();
+    }else{
+        pwdRegex.setCustomValidity('');
+    }
+});
 
-    password1 === password2 ? console.log('Ok') : console.log('NoGo');
+const pwdValidated = document.getElementById('pwdVal');
+
+pwdValidated.addEventListener('input', (e) => {
+
+    let pwdString = pwdValidated.value;
+    let pwdFirst = document.getElementById('pwd').value;
+
+    if(pwdString != pwdFirst){
+        pwdValidated.setCustomValidity('Passwords do not match');
+        pwdValidated.reportValidity();
+    }else{
+        pwdValidated.setCustomValidity('');
+    }
+});
+
+function submitCheck(){
+    let first = pwdRegex.value;
+    let second = pwdValidated.value;
+    if(first == second){
+    }
+    else{
+        alert('Passwords do not match');
+    }
 }
